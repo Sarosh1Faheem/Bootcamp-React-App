cdae0ffa1c2c515504a4b30296677e77f14df30c
Sarosh Faheem <52979134+Sarosh1Faheem@users.noreply.github.com>
2020-06-16 05:56:05 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, World.!, From Sarosh
+          Hello, World.!, From Sarosh, Student Of PIAIC
         </p>
         <a
           className="App-link"
